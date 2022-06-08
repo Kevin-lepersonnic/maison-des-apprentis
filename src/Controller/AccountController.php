8c3c28053a736776d903280c7e7e3b977d201203
Florian <92161528+peflor@users.noreply.github.com>
2022-06-08 11:43:47 +0200

    création du Form OK

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('account/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/account/new', name: 'user_create')]
+    public function create(Request $request, EntityManagerInterface $manager){
+
+            $user= new User();
+            $form = $this->createForm(AccountType::class,$user);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash("success","Bienvenue <strong>{$user->getFullname()}</strong>, votre compte a bien été créé");
+
+                return $this->redirectToRoute('user_show',[
+                    'slug' => $user->getSlug()]);
+            }
+
+        return $this->render('account/create.html.twig', [
+            'form'=> $form->createView()
+        ]);
+    }
+
+    
+    #[Route('/account/{slug}/edit', name: 'user_edit')]
+    public function edit(Request $request, User $user, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(AccountType::class, $user);
+        $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $manager->flush();
+
+                $this->addFlash("info","Votre compte <strong>{$user->getFullname()}</strong> a bien été modifié");
+
+                return $this->redirectToRoute('user_show',[
+                    'slug' => $user->getSlug()]);
+            }
+
+        return $this->render('account/edit.html.twig', [
+            'user' => $user,
+            'form'=> $form->createView()
+        ]);
+
+    }
+
+    #[Route('/account/{slug}/delete', name: 'user_delete')]
+    public function delete(User $user, EntityManagerInterface $manager)
+    {
+        $manager->remove($user);
+        $manager->flush();
+
+        $this->addFlash("danger","Votre compte a bien été supprimé");
+
+
+        return $this->redirectToRoute('articles_index');
+
+    }
+
+    #[Route('/account/{slug}', name: 'user_show')]
+    public function show($slug, UserRepository $userRepository)
+    {        
+        $user = $userRepository->findOneBySlug($slug);
+        return $this->render('account/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+}
---
+++ src/Form/AccountType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('firstname', TextType::class, [
+            'label'=> "Prenom",
+            "attr" => ['placeholder'=>'Votre prénom'],
+        ])
+        ->add('lastname', TextType::class, [
+            'label'=> "Nom",
+            "attr" => ['placeholder'=>'Votre nom'],
+        ])
+        ->add('email', EmailType::class, [
+            'label'=> "Email",
+            "attr" => ['placeholder'=>'Votre email'],
+        ])
+        ->add('hash', PasswordType::class, [
+            'label'=> "Mot de passe",
+            "attr" => ['placeholder'=>'Votre mot de passe'],
+        ])
+        ->add('passwordConfirm', PasswordType::class, [
+            'label'=> "Confirmation du mot de passe",
+            "attr" => ['placeholder'=>'Retapez votre mot de passe'],
+        ])
+        ->add('avatar', TextType::class, [
+            'label'=> "Lien de votre image",
+            "attr" => ['placeholder'=>'Coller un lien d\'image'],
+        ])
+        ->add('Envoyer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
