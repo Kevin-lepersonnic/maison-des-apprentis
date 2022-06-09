be8948f999c3a0de4014e3208d069d6811158801
Florian <92161528+peflor@users.noreply.github.com>
2022-06-09 17:21:35 +0200

    update suite au merge

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -28,16 +28,12 @@ class Article
     #[Assert\NotBlank(message:"Ce champs ne peut pas etre vide")]
     private $title;
 
-<<<<<<< Updated upstream
     #[ORM\Column(type: 'text')]
     #[Assert\Length(
         min: 10,
         minMessage: 'Minimum {{ limit }} caractères',
     )]
     #[Assert\NotBlank(message:"Ce champs ne peut pas etre vide")]
-=======
-    #[ORM\Column(type: 'text', length: 255)]
->>>>>>> Stashed changes
     private $content;
 
     #[ORM\Column(type: 'datetime')]
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,7 +6,6 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
